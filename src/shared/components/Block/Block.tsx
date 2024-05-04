524aa383dad8fe94f8ce40b6b5fa58f9d93f6747
neolates <ddoslang@gmail.com>
2024-05-04 21:58:24 +0200

    next block in progress

--- src/shared/components/Block/Block.tsx
+++ src/shared/components/Block/Block.tsx
@@ -3,6 +3,10 @@ import BlockGraphs from "./BlockGraphs/BlockGraphs";
 import BlockHeader from "./BlockHeader/BlockHeader";
 import BlockInfo from "./BlockInfo/BlockInfo";
 import "./Block.scss";
+import IconArrowDown from "../../icons/IconArrowDown";
+import IconArrowUp from "../../icons/IconArrowUp";
+import { useState } from "react";
+import BlockBet from "./BlockBet/BlockBet";
 
 export enum BlockState {
   "on_bet",
@@ -41,6 +45,14 @@ const Block = ({
   up_bet_sum,
   down_bet_sum,
 }: IBlock) => {
+  const [controlOpen, setControlOpen] = useState<boolean | "UP" | "DOWN">(
+    false
+  );
+
+  const onBetClickHandler = (side: "UP" | "DOWN") => {
+    setControlOpen(side);
+  };
+
   return (
     <div className="block">
       <BlockHeader state={state} />
@@ -60,13 +72,38 @@ const Block = ({
           />
         </div>
         <div className="block__info">
-          <BlockInfo
-            last_price={current_price}
-            locked_price={locked_price}
-            prize_pool={up_bet_sum + down_bet_sum}
-            change={locked_price - current_price}
-          />
+          {state === "on_bet" ? (
+            <div className="block__prediction">
+              <p className="block__prediction-title">Make Your Prediction</p>
+
+              <div className="block__prediction-buttons">
+                <div
+                  onClick={() => onBetClickHandler("UP")}
+                  className="block__prediction-button block__prediction-button_red"
+                >
+                  <IconArrowUp />
+                  DOWN
+                </div>
+                <div
+                  onClick={() => onBetClickHandler("DOWN")}
+                  className="block__prediction-button block__prediction-button_green"
+                >
+                  <IconArrowDown />
+                  UP
+                </div>
+              </div>
+            </div>
+          ) : (
+            <BlockInfo
+              last_price={current_price}
+              locked_price={locked_price}
+              prize_pool={up_bet_sum + down_bet_sum}
+              change={locked_price - current_price}
+            />
+          )}
         </div>
+
+        {controlOpen && <BlockBet />}
       </div>
     </div>
   );
---
+++ src/shared/components/Block/BlockBet/BlockBet.tsx
@@ -0,0 +1,14 @@
+import "./BlockBet.scss";
+
+const BlockBet = () => {
+  return (
+    <div className="block-bet">
+      <div className="block-bet__header">
+        <p className="block-bet__title">Set prediction UP</p>
+      </div>
+      <div className="block-bet__content"></div>
+    </div>
+  );
+};
+
+export default BlockBet;
